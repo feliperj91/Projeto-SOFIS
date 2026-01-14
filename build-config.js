8eaebba4c2577409771bd3c1a58bbb5127ea459c
Felipe <fcalmeida23@gmail.com>
2026-01-14 19:47:12 -0300

    chore(release): version 2.0.2 with updated modal styles and cleaner changelog

--- build-config.js
+++ build-config.js
@@ -1,7 +1,7 @@
 // Centralized Build Version Configuration
 // This file is the single source of truth for the application version
 window.SOFIS_BUILD = {
-    version: '2.0.0',
+    version: '2.0.2',
     date: '2026-01-14',
-    time: '19:10'
+    time: '19:45'
 };
