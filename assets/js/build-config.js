3ccbc931ce7b3c5825bcb8d9afe3127192696c26
Felipe <fcalmeida23@gmail.com>
2026-01-19 00:17:21 -0300

    docs: bump version to 2.1.0 and create changelog

--- assets/js/build-config.js
+++ assets/js/build-config.js
@@ -1,7 +1,7 @@
 // Centralized Build Version Configuration
 // This file is the single source of truth for the application version
 window.SOFIS_BUILD = {
-    version: '2.0.9',
-    date: '2026-01-18',
-    time: '15:17'
+    version: '2.1.0',
+    date: '2026-01-19',
+    time: '00:16'
 };
